c8c98bf7664e4239ba25841835c2fe22e3a45a04
Sammy Ndabo <ndabosam084@gmail.com>
2023-09-24 18:12:27 +0200

    JWT Auth & User create

---
+++ src/main/java/com/cyberclick/universityStudentSrsBackend/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.cyberclick.universityStudentSrsBackend.repository;
+import com.cyberclick.universityStudentSrsBackend.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
