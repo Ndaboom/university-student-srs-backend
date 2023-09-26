0ba13f7bd321df8d977548f9b9e002569766ebbd
Sammy Ndabo <ndabosam084@gmail.com>
2023-09-27 00:45:24 +0200

    Update UserController.java

--- src/main/java/com/cyberclick/universityStudentSrsBackend/controller/UserController.java
+++ src/main/java/com/cyberclick/universityStudentSrsBackend/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173/", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 public class UserController {
