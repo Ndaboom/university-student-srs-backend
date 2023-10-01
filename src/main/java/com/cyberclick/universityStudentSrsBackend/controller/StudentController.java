dbc16f44e7a1b699c7fc7b08e1d701c5ac3e9863
Sammy Ndabo <ndabosam084@gmail.com>
2023-10-01 15:49:43 +0200

    Added studentImage property

--- src/main/java/com/cyberclick/universityStudentSrsBackend/controller/StudentController.java
+++ src/main/java/com/cyberclick/universityStudentSrsBackend/controller/StudentController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
